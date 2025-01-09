6da709e313de200a3e29fc6102b10ca5eff47197
Leonardo Castro <alexchristie1988@gmail.com>
2025-01-09 20:30:46 -0300

    mas especificaciones de for

--- control-flujo/SentenciaFor.java
+++ control-flujo/SentenciaFor.java
@@ -3,7 +3,7 @@ class SentenciaFor {
         //Es el for de toda la vida
         int tabla = 2;
         for(int i = 0; i <= 10; i++) {
-            System.out.println(tabla + "x" + i + "=" + (tabla*i));
+            //System.out.println(tabla + "x" + i + "=" + (tabla*i));
 
             //Esto no es del curso, pero existe una manera de hacer algo parecido a un template literal que es usar la el metodo de clase String.format,
             //Se establecen identificadores de variables con los strings, el string principal es el primer argument y los demas son las variables
@@ -15,6 +15,43 @@ class SentenciaFor {
             */
            //Esto es de java 1.8 porque de java 13 en adelante hay una manera mas sencilla de hacerlo
             System.out.println(String.format("%d x %d = %d", tabla, i, (tabla*i)));
+        }
+
+        System.out.println("=========================");
+
+        //Existen 2 sentencias que sirven para modificar el comportamiento de un for en java continue y break
+        //====continue: si se cumple una condicion o se llama continue antes de otra instruccion, se pasa a la siguiente iteracion sin realizar el resto de las instrucciones
+        //====break: si se cumple una condicion o se llama break, se detienen todas las iteraciones posteriores
+        for(int i = 0; i <= 10; i++) {
+                if(i%2 == 0) {
+                    //break;
+                    continue;
+                }
+            System.out.println(String.format("%d x %d = %d", tabla, i, (tabla*i)));
+        }
+
+        System.out.println("=========================");
+
+        //Se pueden anidar ciclos
+        for(int j = 0; j <= 10; j++) {
+            System.out.println("///////////////////////");
+            for(int i = 0; i <= 10; i++) {
+                System.out.println(String.format("%d x %d = %d", tabla, i, (tabla*i)));
+            }
+        }
+
+        System.out.println("=========================");
+
+        //Con ciclos anidados, se le pueden poner tags a los ciclos for como si fueran nombres de variables, entonces cuando se ejcute un break o continue puedes decir que tag estas deteniendo
+        outer: for(int j = 0; j <= 10; j++) {
+            System.out.println("///////////////////////");
+            inner: for(int i = 0; i <= 10; i++) {
+                if(i == 9) {
+                    System.out.println("Breaking outer");
+                    break outer;
+                }
+                System.out.println(String.format("%d x %d = %d", tabla, i, (tabla*i)));
+            }
         }
     }
 }
